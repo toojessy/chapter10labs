6709216771360558190d42b44e97d9d9fa5b2223
toojessy <jnkamienski@my.waketech.edu>
2025-10-24 08:45:44 -0400

    Update UML.java

--- UML.java
+++ UML.java
@@ -1,27 +1,49 @@
 // Jessica Kamienski
 // 10/24
-
-public class UML {
   
-public Book() {
-  String trackTitle;
+public class Book {
+  String title;
   String author;
   String pages;
   String genre;
+
+  public Book(String title, String author, String genre) {
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+  }
 }
-public BankAccount() {
-  Double balance;
-  String accountHolder;
+public class BankAccount {
+  private double balance;
+  private String accountHolder;
+  private String accountNumber;
+
+  public BankAccount(String accountNumber, double balance) {
+    this.accountNumber = accountNumber;
+    this.balance = balance;
+  }
 }
-public Movie() {
+public class Movie {
   String title;
   String rating;
-  Int playCount;
+  int playCount;
+  double ticketPrice;
+
+  public Movie(String title, String rating, double ticketPrice) {
+    this.title = title;
+    this.rating = rating;
+    this.ticketPrice = ticketPrice;
+  }
 }
-public Rectangle() {
-  Int width;
-  Int height;
-  Int area;
-  Double perimeter;
+public class Rectangle {
+  int width;
+  int height;
+  int area;
+  double perimeter;
+
+  public Rectangle(int width, int area) {
+    this.width = width;
+    this.area = area;
+  }
 }
-}
+
