5489e3b4b97bc2db82c98c90b1166e2678afa708
toojessy <jnkamienski@my.waketech.edu>
2025-10-24 08:12:25 -0400

    Update UML.java

--- UML.java
+++ UML.java
@@ -1,2 +1,27 @@
 // Jessica Kamienski
 // 10/24
+
+public class UML {
+  
+public Book() {
+  String trackTitle;
+  String author;
+  String pages;
+  String genre;
+}
+public BankAccount() {
+  Double balance;
+  String accountHolder;
+}
+public Movie() {
+  String title;
+  String rating;
+  Int playCount;
+}
+public Rectangle() {
+  Int width;
+  Int height;
+  Int area;
+  Double perimeter;
+}
+}
