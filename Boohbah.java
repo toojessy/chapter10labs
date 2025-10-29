39e41333a095d66572daf8632c11a7da82c52c7b
toojessy <jnkamienski@my.waketech.edu>
2025-10-29 03:24:35 -0400

    Update Boohbah.java

--- Boohbah.java
+++ Boohbah.java
@@ -1,1 +1,29 @@
+// Jessica Kamienski
 
+public class Boohbah {
+    private String name;
+    private String danceMove;
+    
+    public Boohbah(String name, String danceMove) {
+         this.name = name;
+         this.danceMove = danceMove;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getDanceMove() {
+        return danceMove;
+    }
+    
+    public String performMove() {
+        return name + " performs " + danceMove;
+        
+    }
+
+// tests the code
+    public static void main(String[] args) {
+        Boohbah b = new Boohbah("Zing Zing Zingbah", "Spin and Sparkle");
+        System.out.println(b.performMove());
+}
+  //
+}
