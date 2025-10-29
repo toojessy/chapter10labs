d1d8c16e0110328eeefe4097b6046fce50bedacc
toojessy <jnkamienski@my.waketech.edu>
2025-10-29 04:01:50 -0400

    Update DanceRoutine.java

--- DanceRoutine.java
+++ DanceRoutine.java
@@ -20,21 +20,4 @@ public String modifyRoutine() {
     String result = modified.toString().replace("performs", "dances to");
     return result;
 }
-
-// test code
-public static void main(String[] args) {
-        Boohbah b1 = new Boohbah("Zing Zing Zingbah", "Spin and Sparkle");
-        Boohbah b2 = new Boohbah("JingBah", "Bouncing Poof");
-        Boohbah b3 = new Boohbah("JumBah", "Whirly Weaving");
-
-        Boohbah[] myBoohbahs = {b1, b2, b3};
-        DanceRoutine routine = new DanceRoutine(myBoohbahs);
-
-        System.out.println("Original Routine:");
-        System.out.println(routine.buildRoutine());
-
-        System.out.println("Modified Routine:");
-        System.out.println(routine.modifyRoutine());
-    }
-//
 }
